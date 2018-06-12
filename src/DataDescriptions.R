ba34fe7e71a9041b845c62558aa481f77c40453a
Katerina Giannoutsou <katerinag@dhcp-18-189-95-165.dyn.MIT.EDU>
2018-06-12 12:03:03 -0400

    Initiation of data description file in R

---
+++ src/DataDescriptions.R
@@ -0,0 +1,3 @@
+LDS_Cardio_NewModel <- read_delim("/mount_point/LDS_Cardio_NewModel.txt",";", escape_double = FALSE, trim_ws = TRUE)
+Cardio_3_5 <- read_delim("/mount_point/LDS_Model3_5_Cardio.csv.txt",";", escape_double = FALSE, trim_ws = TRUE)
+Cardio_6 <- read_delim("/mount_point/LDS_Model6_Cardio.txt",";", escape_double = FALSE, trim_ws = TRUE)
